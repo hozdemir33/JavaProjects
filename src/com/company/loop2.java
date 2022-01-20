99963198d5655f004fef373c6fade8d0812df484
hozdemir33 <hozdemir8233@gmail.com>
2022-01-20 10:47:42 -0800

    initial commit

---
+++ src/com/company/loop2.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class loop2 {
+
+    public static void main(String[] args) {
+
+        //Convert "Java" to "J*a*v*a*"
+
+        String name="Java";
+        String name2="";
+
+        for(int i=0; i<name.length();i++){
+
+            char kk=  name.charAt(i);
+
+            System.out.print(kk+"*"); //0 1 2 3
+
+               }
+            }
+        }
+
+
+
