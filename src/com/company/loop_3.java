56099f4d38401fd3d829443d2449249c2e49691f
hozdemir33 <hozdemir8233@gmail.com>
2022-01-20 15:41:27 -0800

    initial commit

---
+++ src/com/company/loop_3.java
@@ -0,0 +1,69 @@
+package com.company;
+
+import java.io.PrintStream;
+
+public class loop_3 {
+
+    public static void main(String[] args) {
+
+        String letters = "abcdefghijklmnopqrstuvwxyz";
+        String phrase = "This is a phrase!";
+        //  System.out.println("The following shows the letter frequencies for the phrase");
+        for (int i = 0; i < letters.length(); i++) {
+              char kk=letters.charAt(i);
+
+              int counter=0;
+
+            for(int j=0;j<phrase.length();i++){
+
+            counter=counter+phrase.charAt(j);
+
+            if(kk==counter){
+              counter++;
+
+            }
+
+
+
+
+
+
+
+            }
+
+
+        }
+    }
+
+}
+
+
+
+
+/*
+It’s Your Turn
+Another example of nested iteration with string variables is frequency analysis. In frequency analysis, you analyze a message by how often the most common letters (like E and T) occur in the message. This is a useful tool in cybersecurity, particularly in the field of cryptography.
+
+Your task is to write a program, LetterFrequencies, that prints each letter of the alphabet and the number of times that letter occurs in the phrase. To complete this task, you will need to incorporate the following constructs.
+
+Loop to iterate through the alphabet (the outer loop)
+Loop to iterate through all the letters of a phrase (the inner loop)
+Counter variable to count the number of times a letter is in the phrase
+Print statement to display the frequencies/*
+
+HIDE ---
+
+Create the outer loop first and make sure it works, then create the inner loop.
+You can use the substring method to get individual letters.
+If you’re using a while loop, don’t forget to increment!
+You will need an if statement to check if values are equal in order to add to the counter.
+Don’t forget to reset the counter after each iteration.
+
+
+
+ * Activity 2.4.4
+ */
+
+
+
+
