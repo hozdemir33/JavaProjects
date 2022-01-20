31d4fe7d9b246bbfef14bb0a31194c8abf980697
hozdemir33 <hozdemir8233@gmail.com>
2022-01-20 00:20:19 -0800

    initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+     String a="Hasan";
+     String b="Ozdemir";
+
+     //What is String (a+b)?
+
+        System.out.println(a+" "+b);
+
+
+    }
+}
