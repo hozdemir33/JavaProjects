865b46c4ad73cf25a1f8a1193fc32ae61e555978
hozdemir33 <hozdemir8233@gmail.com>
2022-01-20 22:42:54 -0800

    initial commit

---
+++ src/com/company/loop_5.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class loop_5 {
+
+    public static void main(String[] args) {
+
+
+        String s="example";
+
+        for(int i=0;i<s.length();i++){
+
+            String mm=s.substring(i,i+1);
+
+            System.out.print(mm+"");
+
+        }
+
+    }
+
+}
