e477313f241d97f3032543d3e2eb02865650fc8b
hozdemir33 <hozdemir8233@gmail.com>
2022-01-21 20:05:07 -0800

    inital commit

--- src/com/company/loop_6.java
+++ src/com/company/loop_6.java
@@ -5,22 +5,38 @@ public class loop_6 {
 
     public static void main(String[] args) {
 
-        String name= "Strawberry";  //finding the uniques elements from a String...
+        String name = "Strawberry";  //finding the uniques elements from a String...
 
-        for(int i=0; i<name.length();i++){
+        for (int i = 0; i < name.length(); i++) {
 
-            char newContainer=name.charAt(i);
+            char newContainer = name.charAt(i);
 
-            if(name.indexOf(newContainer)==name.lastIndexOf(newContainer)){
+            if (name.indexOf(newContainer) == name.lastIndexOf(newContainer)) {
 
                 System.out.print(newContainer);
+
+
+                //Type code to print a String in reverse after removing spaces
+                //   Example: Ali Can ==> naCilA
+
+
+                String name1 = "Ali Can";
+                String container = "";
+
+                for (int j = name1.length() - 1; j >= 0; j--) {
+                    char kk = name1.charAt(j);
+
+                    if (kk != ' ') {
+
+                        container = container + kk;
+
+
+                    }
+                    System.out.println(container);
+                }
+
+
             }
-
-
-
-
         }
-
-
     }
 }
---
+++ src/com/company/loop_7.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class loop_7 {
+
+    public static void main(String[] args) {
+
+        //We can use StringBuffer to REVERSE ANY STRING VALUES;
+
+        StringBuffer name2=new StringBuffer("Ali Can");
+        name2.reverse();
+        System.out.println(name2);
+
+        //We can also use StringBuilder to REVERSE ANY STRING VALUES;
+
+        StringBuilder name3=new StringBuilder("YUNUF");
+        name3.reverse();
+        System.out.println(name3);
+
+   }
+}
